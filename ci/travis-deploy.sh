c8b39095f0d6a7dd03e0c17a97562ba6dfd6e82e
Russel Waters <russel@nano.org>
2019-10-08 21:50:19 -0400

    attempt to guess at path for awscli

--- ci/travis-deploy.sh
+++ ci/travis-deploy.sh
@@ -12,6 +12,7 @@ elif [[ "$OS" == 'Darwin' ]]; then
     aws s3 cp $TRAVIS_BUILD_DIR/build/nano_pow_server-*-Darwin.tar.gz s3://$AWS_BUCKET/pow-server/$TRAVIS_BRANCH/nano_pow_server-$TRAVIS_COMMIT-Darwin.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
     aws s3 cp $TRAVIS_BUILD_DIR/build/nano_pow_server-*-Darwin.tar.gz s3://$AWS_BUCKET/pow-server/$TRAVIS_BRANCH/nano_pow_server-latest-Darwin.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 else
-	aws s3 cp $TRAVIS_BUILD_DIR/build/nano_pow_server-*-win64.tar.gz s3://$AWS_BUCKET/pow-server/$TRAVIS_BRANCH/nano_pow_server-$TRAVIS_COMMIT-win64.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-	aws s3 cp $TRAVIS_BUILD_DIR/build/nano_pow_server-*-win64.tar.gz s3://$AWS_BUCKET/pow-server/$TRAVIS_BRANCH/nano_pow_server-latest-win64.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+
+	/c/Program\ Files/Amazon/AWSCLI/bin/aws s3 cp $TRAVIS_BUILD_DIR/build/nano_pow_server-*-win64.tar.gz s3://$AWS_BUCKET/pow-server/$TRAVIS_BRANCH/nano_pow_server-$TRAVIS_COMMIT-win64.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+	/c/Program\ Files/Amazon/AWSCLI/bin/aws s3 cp $TRAVIS_BUILD_DIR/build/nano_pow_server-*-win64.tar.gz s3://$AWS_BUCKET/pow-server/$TRAVIS_BRANCH/nano_pow_server-latest-win64.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 fi
